4cc02ecb60d5ab6eb324b530b3bdcdc2c39ab80e
3mpty <aurelien.corre@outlook.fr>
2024-07-15 10:59:20 +0200

    Trying some config stuff

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  },
   base: process.env.BASE_URL || '/',
   resolve: {
     alias: {
